0d751e1d4f15e0a9df1f42a1cd444aba30802802
PillowPillow <gaignoux.nicolas@gmail.com>
2019-01-15 10:31:38 +0100

    [ng-common-tools] add array helpers, bump to v1.3.0

--- projects/ng-common-tools/src/lib/helpers/array.spec.ts
+++ projects/ng-common-tools/src/lib/helpers/array.spec.ts
@@ -7,4 +7,23 @@ describe('Helpers : ArrayHelper', () => {
         expect(nums.sort()).toEqual([1, 2, 3]);
     });
 
+    describe('isArrayOf()', () => {
+
+        it('should return true if the given parameter meet the requirements', () => {
+            expect(ArrayHelper.isArrayOf(['item'], 1)).toBeTruthy();
+        });
+
+        it('should return false if the given array do not meet the required length', () => {
+            expect(ArrayHelper.isArrayOf(['item'], 2)).toBeFalsy();
+        });
+
+        it('should return false if the given parameter is not an array', () => {
+            expect(ArrayHelper.isArrayOf({}, 0)).toBeFalsy();
+            expect(ArrayHelper.isArrayOf(1, 0)).toBeFalsy();
+            expect(ArrayHelper.isArrayOf('', 0)).toBeFalsy();
+            expect(ArrayHelper.isArrayOf('string', 6)).toBeFalsy();
+        });
+
+    });
+
 });
--- projects/ng-common-tools/src/lib/helpers/array.ts
+++ projects/ng-common-tools/src/lib/helpers/array.ts
@@ -5,4 +5,7 @@ export class ArrayHelper {
         return array.filter((data: any, index: number) => array.indexOf(data) === index);
     }
 
+    static isArrayOf(subject: any, expectedLength: number) {
+        return Array.isArray(subject) && subject.length === expectedLength;
+    }
 }
