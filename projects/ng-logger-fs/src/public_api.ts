7639060c119d74feac208d789d5458836b2e3442
PillowPillow <gaignoux.nicolas@gmail.com>
2019-01-28 10:25:05 +0100

    remove default lib files from ng logger fs lib

--- projects/ng-logger-fs/src/public_api.ts
+++ projects/ng-logger-fs/src/public_api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of ng-logger-fs
  */
-
-export * from './lib/ng-logger-fs.service';
-export * from './lib/ng-logger-fs.component';
-export * from './lib/ng-logger-fs.module';
