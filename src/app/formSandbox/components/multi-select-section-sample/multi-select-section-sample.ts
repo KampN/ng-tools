ece35dea7f76bf6a899fb3e16ec492561409d9b3
PillowPillow <gaignoux.nicolas@gmail.com>
2019-02-27 18:20:59 +0100

    add multi select section sample to the sandbox env

---
+++ src/app/formSandbox/components/multi-select-section-sample/multi-select-section-sample.ts
@@ -0,0 +1,29 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {RxCleaner} from '@kamp-n/ng-common-tools';
+
+@Component({
+    selector: 'multi-select-section-select-sample',
+    templateUrl: './template.html',
+    styleUrls: ['./style.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MultiSelectSectionSampleComponent implements OnInit, OnDestroy {
+    form: FormGroup;
+    protected rc: RxCleaner = new RxCleaner();
+
+    constructor(protected fb: FormBuilder, protected cdr: ChangeDetectorRef) {
+    }
+
+    ngOnInit() {
+        this.form = this.fb.group({
+            states: this.fb.control(['A'])
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.rc.complete();
+    }
+
+}
