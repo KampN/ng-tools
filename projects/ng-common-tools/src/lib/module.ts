b0379816008539e1b2e3fd99e856e3f7ab595ebf
PillowPillow <gaignoux.nicolas@gmail.com>
2019-03-04 15:30:57 +0100

    [ng-common-tools] add scrolling module

--- projects/ng-common-tools/src/lib/module.ts
+++ projects/ng-common-tools/src/lib/module.ts
@@ -2,9 +2,13 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Pipes} from './pipes/index';
 import {Components} from './components/index';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        ScrollingModule
+    ],
     exports: [...Pipes, ...Components],
     declarations: [...Pipes, ...Components],
     providers: [],
