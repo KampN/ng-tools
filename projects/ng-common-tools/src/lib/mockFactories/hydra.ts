0dabc018017de65da43dd9cbc68162ca9a94e043
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2019-07-15 20:06:39 +0200

    fix hydra helper typing

--- projects/ng-common-tools/src/lib/mockFactories/hydra.ts
+++ projects/ng-common-tools/src/lib/mockFactories/hydra.ts
@@ -1,29 +1,29 @@
-import {HydraCollection} from '../network/interfaces/hydra';
+import {HydraCollection, HydraItem} from '../network/interfaces/hydra';
 import * as faker from 'faker';
 
 // @dynamic
 export class HydraFactory {
 
-    static generateCollection(items: any[], total: number = items.length, type: string = faker.random.word()): HydraCollection {
-        return {
-            '@context': `/fake/contexts/${type}`,
-            '@id': `/fake/${type}`,
-            '@type': 'hydra:Collection',
-            'hydra:member': items.map((item) => HydraFactory.generateItem(item, type)),
-            'hydra:totalItems': total,
-            'hydra:view': {
-                '@id': `/fake/${type}`,
-                '@type': 'hydra:Collection'
-            }
-        };
-    }
+	static generateCollection(items:any[], total:number = items.length, type:string = faker.random.word()):HydraCollection {
+		return {
+			'@context': `/fake/contexts/${type}`,
+			'@id': `/fake/${type}`,
+			'@type': 'hydra:Collection',
+			'hydra:member': items.map((item) => HydraFactory.generateItem(item, type)),
+			'hydra:totalItems': total,
+			'hydra:view': {
+				'@id': `/fake/${type}`,
+				'@type': 'hydra:Collection'
+			}
+		};
+	}
 
-    static generateItem(item: any, type: string = faker.random.word()): HydraCollection {
-        return Object.assign({
-            '@context': `/fake/contexts/${type}`,
-            '@id': 'id' in item ? `/fake/${type}/${item.id}` : null,
-            '@type': type
-        }, item);
-    }
+	static generateItem(item:any, type:string = faker.random.word()):HydraItem {
+		return Object.assign({
+			'@context': `/fake/contexts/${type}`,
+			'@id': 'id' in item ? `/fake/${type}/${item.id}` : null,
+			'@type': type
+		}, item);
+	}
 
 }
--- projects/ng-common-tools/src/lib/network/helpers/hydra.ts
+++ projects/ng-common-tools/src/lib/network/helpers/hydra.ts
@@ -6,32 +6,33 @@ export type HydraId = string;
 
 export class Hydra {
 
-    static id(item: HydraId | object): HydraId {
-        if (typeof item === 'string') return item;
-        if (Check.propertyExists(item, '@id')) return item['@id'];
-        return null;
-    }
+	static id(item:HydraId | object):HydraId {
+		if(typeof item === 'string') return item;
+		if(Check.propertyExists(item, '@id')) return item['@id'];
+		return null;
+	}
 
-    static normalizeResponse(response: HydraItem | HydraCollection): ApiHydraHttpResponse<any> {
-        const metadata: any = {
-            '@context': response['@context'],
-            '@id': response['@id'],
-            '@type': response['@type'],
-        };
-        let data = null;
-        let total: number;
+	static normalizeResponse(response:HydraItem | HydraCollection):ApiHydraHttpResponse<any> {
+		const metadata:any = {
+			'@context': response['@context'],
+			'@id': response['@id'],
+			'@type': response['@type'],
+		};
+		let data = null;
+		let total:number;
 
-        if (response['hydra:member']) {
-            total = response['hydra:totalItems'];
-            data = response['hydra:member'];
-            Object.assign(metadata, {}, response['hydra:view']);
-        } else {
-            data = [response];
-        }
+		if(response['hydra:member']) {
+			total = response['hydra:totalItems'];
+			data = response['hydra:member'];
+			Object.assign(metadata, {}, response['hydra:view']);
+		}
+		else {
+			data = [response];
+		}
 
-        const count = data.length;
-        if (!total) total = count;
-        return {data, metadata, count, total};
-    }
+		const count = data.length;
+		if(!total) total = count;
+		return {data, metadata, count, total};
+	}
 
 }
