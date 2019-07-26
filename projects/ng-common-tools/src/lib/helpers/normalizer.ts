4d46fd2489e496a0c09c3395b69c7a2627b59a84
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2019-07-26 11:32:03 +0200

    update ucfirst - now keep 2ds part case

--- projects/ng-common-tools/src/lib/helpers/normalizer.ts
+++ projects/ng-common-tools/src/lib/helpers/normalizer.ts
@@ -2,10 +2,10 @@ import {Check} from './check';
 
 export class Normalizer {
 
-    static asArray(data?: any | any[]) {
-        if (!Check.isDefined(data)) return [];
-        return data instanceof Array ? data : [data];
-    }
+	static asArray(data?:any | any[]) {
+		if(!Check.isDefined(data)) return [];
+		return data instanceof Array ? data : [data];
+	}
 
 }
 
--- projects/ng-common-tools/src/lib/modules/common/pipes/ucfirst.ts
+++ projects/ng-common-tools/src/lib/modules/common/pipes/ucfirst.ts
@@ -3,10 +3,10 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'ucfirst'})
 export class UCFirstPipe implements PipeTransform {
 
-    public transform(text: string): string {
-        if (!text) return '';
-        return text[0].toUpperCase() + text.slice(1).toLowerCase();
-    }
+	public transform(text:string):string {
+		if(!text) return '';
+		return text[0].toUpperCase() + text.slice(1);
+	}
 
 }
 
