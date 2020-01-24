cd66f4ff8a460b60afc948de0b7fd6d78c929840
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2020-01-24 12:29:06 +0100

    wip

--- projects/ng-common-form/src/lib/common/directives/control-error.ts
+++ projects/ng-common-form/src/lib/common/directives/control-error.ts
@@ -1,7 +1,7 @@
 import {Directive, Host, Input, OnChanges, OnDestroy, OnInit, Optional, SkipSelf, TemplateRef, ViewContainerRef} from '@angular/core';
 import {
-    AbstractControl, AbstractFormGroupDirective, ControlContainer, FormArray, FormArrayName, FormControl, FormGroup, FormGroupDirective,
-    FormGroupName
+	AbstractControl, AbstractFormGroupDirective, ControlContainer, FormArray, FormArrayName, FormControl, FormGroup, FormGroupDirective,
+	FormGroupName
 } from '@angular/forms';
 import {combineLatest, ReplaySubject} from 'rxjs';
 import {distinctUntilChanged, map} from 'rxjs/operators';
@@ -9,110 +9,110 @@ import {RxCleaner} from '@kamp-n/ng-common-tools';
 
 export class ControlErrorDirectiveExceptions {
 
-    static controlNotFound(controlName?: string): Error {
-        return new Error(`*controlError must be used with a valid FormControl. Control "${controlName}" not found.`);
-    }
+	static controlNotFound(controlName?:string):Error {
+		return new Error(`*controlError must be used with a valid FormControl. Control "${controlName}" not found.`);
+	}
 
-    static controlParentNotFound(): Error {
-        return new Error(`*controlError must be used with a parent formGroup directive when using control name as parameter.  You'll want to add a formGroup
+	static controlParentNotFound():Error {
+		return new Error(`*controlError must be used with a parent formGroup directive when using control name as parameter.  You'll want to add a formGroup
        directive and pass it an existing FormGroup instance (you can create one in your class).`);
-    }
+	}
 
-    static ngModelGroup(): Error {
-        return new Error(`*controlError cannot be used with an ngModelGroup parent when using control name as parameter. It is only compatible with parents
+	static ngModelGroup():Error {
+		return new Error(`*controlError cannot be used with an ngModelGroup parent when using control name as parameter. It is only compatible with parents
        that also have a "form" prefix: formGroupName, formArrayName, or formGroup.`);
-    }
+	}
 }
 
 export class ControlErrorContext {
-    public error;
+	public error;
 
-    constructor(public $implicit: string, public errors: any) {
-        this.error = errors[$implicit] || null;
-    }
+	constructor(public $implicit:string, public errors:any) {
+		this.error = errors[$implicit] || null;
+	}
 }
 
 @Directive({
-    selector: '[controlError]'
+	selector: '[controlError]'
 })
 export class ControlErrorDirective implements OnInit, OnChanges, OnDestroy {
-    @Input() controlErrorOf: string | AbstractControl;
-    protected control: AbstractControl;
+	@Input() controlErrorOf:string | AbstractControl;
+	protected control:AbstractControl;
 
-    protected errorStream: ReplaySubject<any> = new ReplaySubject(1);
-    protected rc: RxCleaner = new RxCleaner();
+	protected errorStream:ReplaySubject<any> = new ReplaySubject(1);
+	protected rc:RxCleaner = new RxCleaner();
 
-    constructor(protected vContainer: ViewContainerRef, protected template: TemplateRef<ControlErrorContext>,
-                @Optional() @Host() @SkipSelf() protected parent: ControlContainer) {
-    }
+	constructor(protected vContainer:ViewContainerRef, protected template:TemplateRef<ControlErrorContext>,
+	            @Optional() @Host() @SkipSelf() protected parent:ControlContainer) {
+	}
 
-    ngOnInit(): void {
-        this.errorStream.pipe(
-            distinctUntilChanged(),
-            this.rc.takeUntil('main')
-        ).subscribe((errors) => this.handleControlErrors(errors));
-    }
+	ngOnInit():void {
+		this.errorStream.pipe(
+			distinctUntilChanged(),
+			this.rc.takeUntil('main')
+		).subscribe((errors) => this.handleControlErrors(errors));
+	}
 
-    ngOnChanges() {
-        this.setUpControl();
-        this.listenControlChanges();
-    }
+	ngOnChanges() {
+		this.setUpControl();
+		this.listenControlChanges();
+	}
 
-    ngOnDestroy(): void {
-        this.errorStream.complete();
-        this.rc.complete();
-    }
+	ngOnDestroy():void {
+		this.errorStream.complete();
+		this.rc.complete();
+	}
 
-    protected handleControlErrors(errors: any) {
-        this.vContainer.clear();
-        if (this.isControlInvalid()) {
-            const error = !!errors ? Object.keys(errors).find(() => true) : null;
-            this.vContainer.createEmbeddedView(this.template, new ControlErrorContext(error, errors));
-        }
-    }
+	protected handleControlErrors(errors:any) {
+		this.vContainer.clear();
+		if(this.isControlInvalid()) {
+			const error = !!errors ? Object.keys(errors).find(() => true) : null;
+			this.vContainer.createEmbeddedView(this.template, new ControlErrorContext(error, errors));
+		}
+	}
 
-    protected listenControlChanges() {
-        const control: AbstractControl = this.control;
-        this.rc.unsubscribe('controls');
-        if (!control) return;
+	protected listenControlChanges() {
+		const control:AbstractControl = this.control;
+		this.rc.unsubscribe('controls');
+		if(!control) return;
 
-        this.errorStream.next(control.errors);
-        combineLatest([control.statusChanges, control.valueChanges]).pipe(
-            map(() => control.errors || null),
-            distinctUntilChanged(),
-            this.rc.takeUntil('controls')
-        ).subscribe((errors) => this.errorStream.next(errors));
-    }
+		this.errorStream.next(control.errors);
+		combineLatest([control.statusChanges, control.valueChanges]).pipe(
+			map(() => control.errors || null),
+			distinctUntilChanged(),
+			this.rc.takeUntil('controls')
+		).subscribe((errors) => this.errorStream.next(errors));
+	}
 
-    protected setUpControl() {
-        if (!this.controlErrorOf) return null;
-        let control = null;
+	protected setUpControl() {
+		if(!this.controlErrorOf) return null;
+		let control = null;
 
-        if (this.isFormControl(this.controlErrorOf)) control = this.controlErrorOf as AbstractControl;
-        else if (typeof this.controlErrorOf === 'string') {
-            this.checkParentType();
-            if (this.parent) control = (this.parent as FormGroupDirective).control.get(this.controlErrorOf);
-            if (!control) throw ControlErrorDirectiveExceptions.controlNotFound(this.controlErrorOf);
-        }
+		if(this.isFormControl(this.controlErrorOf)) control = this.controlErrorOf as AbstractControl;
+		else if(typeof this.controlErrorOf === 'string') {
+			this.checkParentType();
+			if(this.parent) control = (this.parent as FormGroupDirective).control.get(this.controlErrorOf);
+			if(!control) throw ControlErrorDirectiveExceptions.controlNotFound(this.controlErrorOf);
+		}
 
-        this.control = control;
-    }
+		this.control = control;
+	}
 
-    protected isFormControl(item: any) {
-        return item instanceof FormControl ||
-            item instanceof FormGroup ||
-            item instanceof FormArray;
-    }
+	protected isFormControl(item:any) {
+		return item instanceof FormControl ||
+			item instanceof FormGroup ||
+			item instanceof FormArray;
+	}
 
-    protected isControlInvalid(): boolean {
-        return this.control && this.control.invalid;
-    }
+	protected isControlInvalid():boolean {
+		return this.control && this.control.invalid;
+	}
 
-    protected checkParentType(): void {
-        if (!(this.parent instanceof FormGroupName) &&
-            this.parent instanceof AbstractFormGroupDirective) throw ControlErrorDirectiveExceptions.ngModelGroup();
-        else if (!(this.parent instanceof FormGroupName) &&
-            !(this.parent instanceof FormGroupDirective) &&
-            !(this.parent instanceof FormArrayName)) throw ControlErrorDirectiveExceptions.controlParentNotFound();
-    }
+	protected checkParentType():void {
+		if(!(this.parent instanceof FormGroupName) &&
+			this.parent instanceof AbstractFormGroupDirective) throw ControlErrorDirectiveExceptions.ngModelGroup();
+		else if(!(this.parent instanceof FormGroupName) &&
+			!(this.parent instanceof FormGroupDirective) &&
+			!(this.parent instanceof FormArrayName)) throw ControlErrorDirectiveExceptions.controlParentNotFound();
+	}
 }
--- projects/ng-logger/src/lib/providers/logDisplay.ts
+++ projects/ng-logger/src/lib/providers/logDisplay.ts
@@ -8,27 +8,27 @@ import {LOGGER_CONFIGURATION} from './configuration';
 @Injectable({providedIn: 'root'})
 export class LogDisplay {
 
-    protected crossFingerEnabled: boolean;
-    protected minLevel: LogLevel;
+	protected crossFingerEnabled:boolean;
+	protected minLevel:LogLevel;
 
-    constructor(protected logStream: LogStream, @Inject(LOGGER_CONFIGURATION) protected config: LoggerConfiguration) {
-        this.crossFingerEnabled = this.config.crossFinger && this.config.crossFinger.enabled;
-        this.minLevel = this.crossFingerEnabled && this.config.crossFinger.level ? this.config.crossFinger.level : 0;
-        this.logStream.log$.pipe(
-            filter((log: LogMessage) => !this.crossFingerEnabled || log.level >= this.minLevel),
-        ).subscribe((log: LogMessage) => this.handleLog(log));
-    }
+	constructor(protected logStream:LogStream, @Inject(LOGGER_CONFIGURATION) protected config:LoggerConfiguration) {
+		this.crossFingerEnabled = this.config.crossFinger && this.config.crossFinger.enabled;
+		this.minLevel = this.crossFingerEnabled && this.config.crossFinger.level ? this.config.crossFinger.level : 0;
+		this.logStream.log$.pipe(
+			filter((log:LogMessage) => !this.crossFingerEnabled || log.level >= this.minLevel),
+		).subscribe((log:LogMessage) => this.handleLog(log));
+	}
 
-    /* istanbul ignore next */
-    showLog(text: string, color: string, data: any) {
-        if (text) console.log(`%c${text}`, `color: ${color}`, data);
-        else console.log(data);
-    }
+	/* istanbul ignore next */
+	showLog(text:string, color:string, data:any) {
+		if(text) console.log(`%c${text}`, `color: ${color}`, data);
+		else console.log(data);
+	}
 
-    protected handleLog(log: LogMessage) {
-        const color: string = this.config.logColors[log.level] || '#808080';
-        this.showLog(log.message, color, log.data);
-        if (this.crossFingerEnabled) this.showLog('Log Stack : ', color, this.logStream.flushStack());
-    }
+	protected handleLog(log:LogMessage) {
+		const color:string = this.config.logColors[log.level] || '#808080';
+		this.showLog(log.message, color, log.data);
+		if(this.crossFingerEnabled) this.showLog('Log Stack : ', color, this.logStream.flushStack());
+	}
 
 }
