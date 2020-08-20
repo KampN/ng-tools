30661ecb2b08a087845288ef454c471fe5ec17ff
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2020-08-20 11:42:36 +0200

    update material search input, allow to disable from input

--- projects/ng-common-form/src/lib/material-ui/material-search-input/material-search-input.ts
+++ projects/ng-common-form/src/lib/material-ui/material-search-input/material-search-input.ts
@@ -1,91 +1,97 @@
 import {
-    AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, forwardRef, Input, OnDestroy, OnInit,
-    Output, ViewChild, ViewEncapsulation
+	AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, forwardRef, Input, OnDestroy, OnInit,
+	Output, ViewChild, ViewEncapsulation
 } from '@angular/core';
 import {FocusMonitor, FocusOrigin} from '@angular/cdk/a11y';
 import {distinctUntilChanged, map} from 'rxjs/operators';
 import {RxCleaner} from '@kamp-n/ng-common-tools';
 import {Subject} from 'rxjs';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {coerceBooleanProperty} from '@angular/cdk/coercion';
 
 @Component({
-    selector: 'material-search-input',
-    templateUrl: './template.html',
-    styleUrls: ['./style.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    encapsulation: ViewEncapsulation.None,
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => MaterialSearchInputComponent),
-            multi: true
-        },
-    ]
+	selector: 'material-search-input',
+	templateUrl: './template.html',
+	styleUrls: ['./style.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => MaterialSearchInputComponent),
+			multi: true
+		},
+	]
 })
 export class MaterialSearchInputComponent implements OnInit, OnDestroy, AfterViewInit, ControlValueAccessor {
 
-    @Input() search: string = '';
-    @Output() searchChange: EventEmitter<string> = new EventEmitter();
-    readonly valueChange: Subject<string> = new Subject();
+	@Input() search: string = '';
+	@Output() searchChange: EventEmitter<string> = new EventEmitter();
+	readonly valueChange: Subject<string> = new Subject();
 
-    @ViewChild('input') input: ElementRef;
-    @Input() placeholder = '';
-    @Input() disableClear: boolean = false;
+	@ViewChild('input') input: ElementRef;
+	@Input() placeholder = '';
+	@Input() disableClear: boolean = false;
 
-    public focused: boolean = false;
-    public disabled: boolean = false;
-    protected rc: RxCleaner = new RxCleaner();
+	public focused: boolean = false;
+	protected rc: RxCleaner = new RxCleaner();
 
-    constructor(protected monitor: FocusMonitor, protected cdr: ChangeDetectorRef) { }
+	constructor(protected monitor: FocusMonitor, protected cdr: ChangeDetectorRef) { }
 
-    get showClearBtn(): boolean {
-        return !this.disableClear && (this.focused || !!this.search);
-    }
+	protected _disabled: boolean = false;
 
-    ngOnInit() {
-        this.valueChange.pipe(
-            distinctUntilChanged(),
-            this.rc.takeUntil('destroy')
-        ).subscribe((value: string) => {
-            this.search = value;
-            this.onTouched();
-            this.onChange(this.search);
-            this.searchChange.emit(this.search);
-        });
-    }
+	get disabled(): boolean {return this._disabled;}
 
-    ngAfterViewInit(): void {
-        this.monitor.monitor(this.input.nativeElement).pipe(
-            map((origin: FocusOrigin) => !!origin),
-            distinctUntilChanged(),
-            this.rc.takeUntil('destroy')
-        ).subscribe((focused: boolean) => {
-            this.focused = focused;
-            this.cdr.markForCheck();
-        });
-    }
+	@Input() set disabled(val: boolean) {this._disabled = coerceBooleanProperty(val);}
 
-    ngOnDestroy(): void {
-        this.rc.complete();
-        this.searchChange.complete();
-        this.valueChange.complete();
-    }
+	get showClearBtn(): boolean {
+		return !this.disableClear && (this.focused || !!this.search);
+	}
 
-    onChange = function(_) { };
+	ngOnInit() {
+		this.valueChange.pipe(
+			distinctUntilChanged(),
+			this.rc.takeUntil('destroy')
+		).subscribe((value: string) => {
+			this.search = value;
+			this.onTouched();
+			this.onChange(this.search);
+			this.searchChange.emit(this.search);
+		});
+	}
 
-    onTouched = function() { };
+	ngAfterViewInit(): void {
+		this.monitor.monitor(this.input.nativeElement).pipe(
+			map((origin: FocusOrigin) => !!origin),
+			distinctUntilChanged(),
+			this.rc.takeUntil('destroy')
+		).subscribe((focused: boolean) => {
+			this.focused = focused;
+			this.cdr.markForCheck();
+		});
+	}
 
-    registerOnChange(fn) { this.onChange = fn; }
+	ngOnDestroy(): void {
+		this.rc.complete();
+		this.searchChange.complete();
+		this.valueChange.complete();
+	}
 
-    registerOnTouched(fn) { this.onTouched = fn; }
+	onChange = function(_) { };
 
-    setDisabledState(isDisabled: boolean): void {
-        this.disabled = isDisabled;
-    }
+	onTouched = function() { };
 
-    writeValue(val: string): void {
-        this.search = val;
-        this.cdr.markForCheck();
-    }
+	registerOnChange(fn) { this.onChange = fn; }
+
+	registerOnTouched(fn) { this.onTouched = fn; }
+
+	setDisabledState(isDisabled: boolean): void {
+		this.disabled = isDisabled;
+	}
+
+	writeValue(val: string): void {
+		this.search = val;
+		this.cdr.markForCheck();
+	}
 
 }
