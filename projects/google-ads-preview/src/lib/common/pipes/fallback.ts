a61d858a2a72beca2bff713a9e48a84030765e2a
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2021-07-02 16:02:59 +0200

    fix build process

--- projects/google-ads-preview/src/lib/common/pipes/fallback.ts
+++ projects/google-ads-preview/src/lib/common/pipes/fallback.ts
@@ -1,6 +1,7 @@
 import {Inject, Optional, Pipe, PipeTransform} from '@angular/core';
 import {GADS_PREVIEW_TRANSLATOR, GAdsPreviewTranslator} from '../providers/translator';
 import {GADS_PREVIEW_VALUE_FORMATTER, GAdsPreviewValueFormatter} from '../providers/value-formatter';
+import {Check} from '@kamp-n/ng-common-tools';
 
 @Pipe({
 	name: 'fallback',
@@ -12,7 +13,7 @@ export class FallbackPipe implements PipeTransform {
 				@Optional() @Inject(GADS_PREVIEW_TRANSLATOR) protected translate: GAdsPreviewTranslator) {}
 
 	transform(value: string, fallback: string, translateFallback: boolean = true): any {
-		if(value != null) return this.format ? this.format(value) : value;
+		if(Check.isDefined(value)) return this.format ? this.format(value) : value;
 		return this.translate && translateFallback ? this.translate(fallback) : fallback;
 	}
 
