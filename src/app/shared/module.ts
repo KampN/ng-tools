f0ae61e7cef154d45c00b48eb104df1e82dc9eed
Guillaume Olivero <guillaume@kampn.com>
2024-06-06 16:41:27 +0200

    [FIX] - HttpClientModule deprecated

--- src/app/shared/module.ts
+++ src/app/shared/module.ts
@@ -1,6 +1,6 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {MaterialModule} from '../material/module';
-import {} from '@angular/common/http';
+import {provideHttpClient} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 import {CommonDevToolsModule, CommonToolsModule} from '@kamp-n/ng-common-tools';
@@ -20,12 +20,13 @@ import {DisplayAdsPreviewModule} from '@kamp-n/gads-preview';
 		DisplayAdsPreviewModule,
 
 		RouterModule,
-		HttpClientModule,
 		MaterialModule,
 		ReactiveFormsModule
 	],
 	declarations: [],
-	providers: [],
+	providers: [
+		provideHttpClient()
+	],
 })
 export class SharedModule {
 	static forRoot():ModuleWithProviders<SharedModule> {
