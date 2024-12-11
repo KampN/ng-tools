0be4d9624c8d6fc6a37fbe86f2d1f53071179dc5
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2024-12-11 11:15:45 +0100

    typo

--- projects/ng-logger/src/lib/logger.providers.ts
+++ projects/ng-logger/src/lib/logger.providers.ts
@@ -1,30 +1,33 @@
-import { EnvironmentProviders, ErrorHandler, InjectionToken, makeEnvironmentProviders, Provider, inject, provideAppInitializer } from '@angular/core';
+import {EnvironmentProviders, ErrorHandler, inject, InjectionToken, makeEnvironmentProviders, provideAppInitializer} from '@angular/core';
 import {configurationFactory, LOGGER_CONFIGURATION} from './providers/configuration';
 import {LoggerConfiguration} from './interfaces/configuration';
 import {LogDisplay} from './providers/logDisplay';
 import {CustomErrorHandler} from './providers/errorHandler';
-import {LogStream} from "./providers/logStream";
+import {LogStream} from './providers/logStream';
 
 const MODULE_CONFIG = new InjectionToken('LOGGER_MODULE_CONFIGURATION');
 
 export function loggerProviders(config?: LoggerConfiguration): EnvironmentProviders {
-	return makeEnvironmentProviders([
-		{ provide: MODULE_CONFIG, useValue: config || {} },
-		{
-			provide: LOGGER_CONFIGURATION,
-			useFactory: configurationFactory,
-			deps: [MODULE_CONFIG]
-		},
-		{
-			provide: ErrorHandler,
-			useClass: CustomErrorHandler
-		},
-		{
-			provide: LogDisplay,
-			deps: [LogStream, LOGGER_CONFIGURATION]
-		},
-		provideAppInitializer((() => {
-				return () => {};
-			})(inject(LogDisplay)))
-	]);
+    return makeEnvironmentProviders([
+        {provide: MODULE_CONFIG, useValue: config || {}},
+        {
+            provide: LOGGER_CONFIGURATION,
+            useFactory: configurationFactory,
+            deps: [MODULE_CONFIG]
+        },
+        {
+            provide: ErrorHandler,
+            useClass: CustomErrorHandler
+        },
+        {
+            provide: LogDisplay,
+            deps: [LogStream, LOGGER_CONFIGURATION]
+        },
+        provideAppInitializer(() => {
+            const initializerFn = ((storage: LogDisplay) => {
+                return () => { };
+            })(inject(LogDisplay));
+            return initializerFn();
+        })
+    ]);
 }
