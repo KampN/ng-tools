cc3b9b259bd525dfbffa4d936af63d12beeff370
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2025-09-23 18:37:42 +0200

    test: refactor test files to improve structure and readability with Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,21 @@ export default defineConfig(({mode}) => ({
     define: {
         'import.meta.vitest': mode !== 'production',
     },
+    reporters: [
+        'dot',                              // ≈ --progress=false (sortie minimale)
+        ['junit', {outputFile: 'coverage/junit.xml'}], // si tu veux un XML CI
+    ],
+    coverage: {
+        provider: 'v8',
+        reportsDirectory: 'coverage',
+        reporter: ['text', 'html', 'lcov'], // équivalent Angular/Karma
+        all: true,                          // inclut fichiers non testés
+        include: ['src/**/*.ts'],
+        exclude: [
+            'src/main.ts',
+            'src/environments/**',
+            'src/**/*.spec.ts',
+            '**/test-setup.ts',
+        ],
+    },
 }));
